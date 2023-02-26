cd0a8d7f8463fddbdfd7105a778d61663303142c
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 02:00:29 +0530

    Update top_Seq_Det_Mealy_0011.v

--- top_Seq_Det_Mealy_0011.v
+++ top_Seq_Det_Mealy_0011.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 10:23:59 PM
-// Design Name: 
-// Module Name: top_Seq_Det_Mealy_0011
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Frequency_Divider(clock_in,clock_out);  // Clock Divider module
