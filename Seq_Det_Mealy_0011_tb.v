498221bfa45718afcbbe4fa784e4632eec3f44d8
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 02:00:47 +0530

    Update Seq_Det_Mealy_0011_tb.v

--- Seq_Det_Mealy_0011_tb.v
+++ Seq_Det_Mealy_0011_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 10:25:31 PM
-// Design Name: 
-// Module Name: Seq_Det_Mealy_0011_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Seq_Det_Mealy_0011_tb;
